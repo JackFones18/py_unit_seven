050757904404969e6bc78f73e7cb185d9462f754
JackFones18 <144365618+JackFones18@users.noreply.github.com>
2023-12-13 09:01:38 -0500

    Update assignment_seven.py

--- assignment_seven.py
+++ assignment_seven.py
@@ -1,11 +1,14 @@
 import time
 import random
 given_sentence = random.randint(0,3)
-chosen_sentence = ["hello", "rahh", "bruh", "ahhh"]
+chosen_sentence = ["The quick brown fox jumps over the lazy dog.",
+                   "Many people like many things, what do you like?",
+                   "Formula one racing has grown in popularity over the years.",
+                   "Over the years, many moons will shine."]
 sentence_to_type = chosen_sentence[given_sentence]
 
 def funtypy():
-    print("This is a typing test. please try to type the following sentence as quickly and as accurately as possible.")
+    print("This is a typing test. Please try to type the following sentence as quickly and as accurately as possible.")
     time.sleep(3)
     print("")
     print("the sentence is:")
@@ -24,19 +27,28 @@ def funtypy():
     print("GO!")
     print(sentence_to_type)
 
-user_type = input("retype that nerd: ")
+def find_accuracy():
+    global user_type
+    user_type = input("Retype the sentence: ")
+    accuracy = 0
+    for user_char, target_char in zip(user_type, sentence_to_type):
+        if user_char == target_char:
+            accuracy += 1
+    accuracy_percentage = (accuracy / len(sentence_to_type)) * 100
+    print("Accuracy:", round(accuracy_percentage,2), "%")
 
 
-if user_type == "Hello":
-    print("yup")
-else:
-    print("no")
-
+def wpm():
+    thing = len(user_type)/5
+    print ("Your WPM is:", thing)
 
 def main():
     funtypy()
     starttime = time.time()
-    totaltime = round((time.time() - starttime), 2)
-    print("Total Time: " + str(totaltime))
 
+    find_accuracy()
+    wpm()
+    global totaltime
+    totaltime = round((time.time() - starttime), 2)
+    print("Time: " + str(totaltime), "seconds.")
 main()
